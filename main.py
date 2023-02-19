7ba14f90a458435887d873615a229e3a3e8c9cdc
MNastri <marcellonastri@gmail.com>
2023-02-19 02:00:14 -0300

    feat/create text for custom scheduler from config

--- main.py
+++ main.py
@@ -1,16 +1,43 @@
-# This is a sample Python script.
+import toml
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+KEYS_OF_DECKPARAMS = (
+    "deckName",
+    "w",
+    "requestRetention",
+    "maximumInterval",
+    "easyBonus",
+    "hardInterval",
+)
+my_config_file = "config.toml"
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f"Hi, {name}")  # Press Ctrl+F8 to toggle the breakpoint.
+def generate_deck_params_code_from(file) -> str:
+    """
+    Creates the deckParams part of the FSRS4Anki custom scheduler code from
+    the given toml fil.e
+    :param file: The toml file
+    :return: the string to be pasted inside the custom scheduler
+    """
+    deck_data = toml.load(file)
+    decks_parameters = ["const deckParams = [\n", "];\n"]
+    for deck_name, deck_params in deck_data.items():
+        deck = [
+            "  {\n",
+            f'    "{KEYS_OF_DECKPARAMS[0]}": "{deck_name}",\n',
+            "  },\n",
+        ]
+        for key_of_deck_params, deck_parameter in zip(
+            KEYS_OF_DECKPARAMS[1:], deck_params.values()
+        ):
+            # insert before the last item in list
+            deck.insert(-1, f'    "{key_of_deck_params}": "{deck_parameter}",\n')
+        deck = "".join(deck)
+        # insert before the last item in list
+        decks_parameters.insert(-1, deck)
+    decks_parameters = "".join(decks_parameters)
+    return decks_parameters
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == "__main__":
-    print_hi("PyCharm")
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    config_str = generate_deck_params_code_from(my_config_file)
+    print(config_str)
